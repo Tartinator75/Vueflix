76f832ff33e4a567dcc871a7e272f2d06b8fb2c4
NicoDev75 <45965028+NicoDev75@users.noreply.github.com>
2020-10-31 18:07:31 +0100

    commit

---
+++ Client/src/configs/api.config.js
@@ -0,0 +1,3 @@
+export default {
+    tapiUrl: process.env.VUE_APP_API_URL
+}
--- Client/src/views/Home.vue
+++ Client/src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <h1>TENTATIVE DE FETCH</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import ApiUsers from "@/mixins/UsersApi.js";
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+  
+  },
+  data() {
+    return {
+      userList: []
+    }
+  },
+   created() {
+            this.getFilms()
+            .then(data => console.log("date: " + date))
+            .catch(err => console.log(err));
+            console.log(this.userList);
+        },
+  mixins:[ApiUsers]
 }
 </script>
