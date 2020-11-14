cabc0acc1fd6a82716b261bf4a880153c974f7c0
RegisGRB <42929082+RegisGRB@users.noreply.github.com>
2020-11-14 22:02:12 +0100

    Update index.js

--- Api/index.js
+++ Api/index.js
@@ -1,4 +1,8 @@
 const app = require('./src/services/express.service');
 const mongoose = require('./src/services/mongoose.service');
 
+
+app.get("/",(req,res)=>{
+    res.send("API IS LOADED");
+});
 mongoose.connect();
